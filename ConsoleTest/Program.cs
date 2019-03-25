5a8fe37f63f967981dfa50195b786b4ed3ce68d6
TRAN Xuan Tien <txtien@gmail.com>
2019-03-25 16:06:38 +0100

    Début

---
+++ ConsoleTest/Program.cs
@@ -0,0 +1,15 @@
+﻿using ITI.DSNTree.Tests;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace TestConsole
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.ReadKey();
+		}
+	}
+}
---
+++ UnitTests/Test.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace ITI.DSNTree.Tests
+{
+	[TestFixture]
+	public class Test
+	{
+		static string GetThisFilePath([CallerFilePath]string path = null) => path;
+		static string GetSolutionDirectory() => Path.GetDirectoryName(Path.GetDirectoryName(GetThisFilePath()));
+		static string GetDataDirectory() => Path.Combine(GetSolutionDirectory(), "Data");
+
+		[Test]
+		public void T00_loading_simple_test_data()
+		{
+		}
+
+		static void CheckFileEquals(string path1, string path2)
+		{
+			File.ReadAllText(path1).Should().Be(File.ReadAllText(path2));
+		}
+	}
+}
